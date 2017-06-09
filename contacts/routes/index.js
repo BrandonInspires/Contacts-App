f53aee64fba0dc9f6273ca96ad64ea391983280c
Brandon Gilchrist <brandongilchrist@LGs-MacBook-Pro-Brandon.local>
2017-06-09 02:02:36 -0700

    Initial Commit

--- contacts/routes/index.js
+++ contacts/routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/contacts', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/contacts/:id', function(req, res, next) {
+  res.render('details', { title: 'Contact Details' });
+});
+
+/* GET home page. */
+router.get('/contacts/new', function(req, res, next) {
+  res.render('new', { title: 'New Contact' });
+});
+
 module.exports = router;
